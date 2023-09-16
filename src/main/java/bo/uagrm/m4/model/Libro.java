8cf2975647029ba8102122a5bacf70714acbe53c
Dev0505 <deved0505@hotmail.com>
2023-09-15 20:17:36 -0400

    updating book model

--- src/main/java/bo/uagrm/m4/model/Libro.java
+++ src/main/java/bo/uagrm/m4/model/Libro.java
@@ -1,5 +1,49 @@
 package bo.uagrm.m4.model;
 
 public class Libro {
-    
+    private int id;
+    private String isbn;
+    private String autor;
+    private String nombre;
+    private String descripcion;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
 }
