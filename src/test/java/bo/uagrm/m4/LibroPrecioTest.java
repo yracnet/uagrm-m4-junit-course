276ad43e7fcb3436e043bbbabe7d262a6b659f1b
Willyams Yujra <yracnet@gmail.com>
2023-09-15 21:21:44 -0400

    + Test 1

---
+++ src/test/java/bo/uagrm/m4/LibroPrecioTest.java
@@ -0,0 +1,37 @@
+package bo.uagrm.m4;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class LibroPrecioTest {
+    
+    public LibroPrecioTest() {
+    }
+
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    
+     @Test
+     public void simple() {     
+         assertEquals("10","1");
+     }
+}
