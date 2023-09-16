d1dbc7a7f4f1e5949b6a1e9ddbee48dfd0c71af6
Willyams Yujra <yracnet@gmail.com>
2023-09-15 20:55:53 -0400

    + Descuento

--- src/main/java/bo/uagrm/m4/model/PrecioLibro.java
+++ src/main/java/bo/uagrm/m4/model/PrecioLibro.java
@@ -4,16 +4,34 @@ import lombok.Data;
 
 @Data
 public class PrecioLibro {
+
     private Long id;
     private String isbn;
     private String edicion;
     private Formato formato;
     private double precioVenta;
-    private double descuento;
-    
-    
-    public static enum Formato{
+    private Descuento descuento;
+
+    public static enum Formato {
         TAPA_DURA,
         TAPA_BLANDA
+    }
+
+    public static enum Descuento {
+        DESC_01(0.01),
+        DESC_05(0.05),
+        DESC_10(0.1),
+        DESC_25(0.25);
+
+        private double valor;
+
+        Descuento(double valor) {
+            this.valor = valor;
+        }
+        
+        public Double cacularDescuento(Double monto){
+            return monto * (1 - valor);
+        }
+
     }
 }
