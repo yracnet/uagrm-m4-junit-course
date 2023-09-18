ce8a69b2c495cc0f30c2eecbd5a2efdaee7fcfc0
Willyams Yujra <yracnet@gmail.com>
2023-09-18 00:01:21 -0400

    + change

--- src/main/java/bo/uagrm/m4/Main.java
+++ src/main/java/bo/uagrm/m4/Main.java
@@ -1,19 +1,17 @@
 package bo.uagrm.m4;
 
 import bo.uagrm.m4.manager.CarritoVentaManager;
+import bo.uagrm.m4.manager.ValorDescuento;
 import bo.uagrm.m4.model.Formato;
 import bo.uagrm.m4.model.TipoPromocion;
 
 public class Main {
     public static void main(String[] args) {
-       
-        
         CarritoVentaManager carrito = new CarritoVentaManager();
         carrito.imprimir();
         carrito.setFechaVentaString("2023-02-07");
         System.out.println(">> 1000, 2010, TAPA_DURA, NUEVO" );
-        var resp = carrito.calcularDescuento("1000", 2010, Formato.TAPA_DURA, TipoPromocion.NUEVO);
+        ValorDescuento resp = carrito.calcularDescuento("1000", 2010, Formato.TAPA_DURA, TipoPromocion.NUEVO);
         resp.imprimir();
     }
-    
 }
